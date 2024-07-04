4cab094aedc0432f9d620a4a9aedfcf6cbbb5ca5
RichardPardosi <richard12eduardo@gmail.com>
2024-07-04 10:29:12 +0700

    update now

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include "koneksi";
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Customer</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="bodylg">
+    <br>
+	<h2><center>Selamat Datang, Silahkan Login.</center></h2>
+
+	<div class="kotak_login">
+		<form method="POST" action="cek.php">
+			<label>Username</label>
+			<input type="text" name="user" class="form_login" placeholder="Masukkan Username Anda..." required>
+
+			<label>Password</label>
+			<input type="password" name="pass" class="form_login" placeholder="Masukkan Password Anda..." required>
+
+			<input type="submit" class="tombol_login" value="LOGIN">
+
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="index.php">Kembali</a>
+			</center>
+		</form>
+	</div>
+
+
+</body>
+</html>
